e216fad530bbb557b14d21883d78ebbc69371802
Dhananjay Varshney <dhananjayvarshney4045@gmail.com>
2025-03-27 12:46:02 +0530

    update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,7 +13,7 @@ const projects = [
   {
     title: "Spotify Website",
     description: "A sleek, Spotify-inspired website with dynamic UI and smooth animations.",
-    image: "/spotify.png", // Ensure correct format
+    image: "/Spotify.png", // Ensure correct format
     link: "https://dhananjay023.github.io/Spotify/",
   },
   {
